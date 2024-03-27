239fe3f7d66c16938cf6f0fe33c0eb5d59f1abb0
MarynaBinko <benyhorses@hotmail.com>
2024-03-27 17:11:18 +0000

    snackbar css

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,33 @@
-console.log(`snack`);
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+let toastBox = document.getElementById(`toastBox`);
+const form = document.getElementById(`notification-form`);
+let successMsg = `<i class="fa-solid fa-square-check"></i>Fulfilled promise in ms`;
+let errorMsg = `<i class="fa-solid fa-xmark"></i>Rejected promise in ms`;
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    showToast();
+});
+
+
+function showToast(){
+  let toast =  document.createElement(`div`);
+  toast.classList.add(`toast`);
+  const selectedState = form.querySelector('input[name="state"]:checked');
+  let value;
+  if (selectedState) {
+    value = selectedState.value;
+  }
+  if (value === "fulfilled") {
+    toast.innerHTML = successMsg;
+    toast.classList.add(`successMsg`);
+  }else{
+    toast.innerHTML = errorMsg;
+    toast.classList.add(`errorMsg`);
+  }
+  
+  toastBox.appendChild(toast);
+}
