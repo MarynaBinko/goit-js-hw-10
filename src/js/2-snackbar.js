bfa613a47dfe57c1636c4da802645a4effe1adac
MarynaBinko <benyhorses@hotmail.com>
2024-03-28 11:06:47 +0000

    snackbar hw10

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,30 +4,51 @@ import "izitoast/dist/css/iziToast.min.css";
 
 let toastBox = document.getElementById(`toastBox`);
 const form = document.getElementById(`notification-form`);
-let successMsg = `<i class="fa-solid fa-square-check"></i>Fulfilled promise in ms`;
-let errorMsg = `<i class="fa-solid fa-xmark"></i>Rejected promise in ms`;
+
+
+function createPromise(){
+return new Promise ((resolve, reject)=>{
+    const delay = document.querySelector('input[name="delay"]').value;
+    const selectedState = form.querySelector('input[name="state"]:checked').value;
+    if(selectedState === "fulfilled"){
+        setTimeout(() => {
+      resolve(delay);      
+        }, delay);
+    }else{
+        setTimeout(() => {
+          reject(delay);  
+        }, delay);
+    }
+})
+}
 
 form.addEventListener("submit", function(event){
     event.preventDefault();
-    showToast();
+    // showToast();
+createPromise()
+.then((delay)=>{
+    const successMsg = `<i class="fa-solid fa-square-check"></i>Fulfilled promise in ${delay}ms`;
+    showToast(successMsg, 'successMsg');
+})
+.catch((delay)=>{
+    const errorMsg = `<i class="fa-solid fa-xmark"></i>Rejected promise in ${delay}ms`;
+    showToast(errorMsg, 'errorMsg');
+})
+
 });
 
 
-function showToast(){
-  let toast =  document.createElement(`div`);
-  toast.classList.add(`toast`);
-  const selectedState = form.querySelector('input[name="state"]:checked');
-  let value;
-  if (selectedState) {
-    value = selectedState.value;
-  }
-  if (value === "fulfilled") {
-    toast.innerHTML = successMsg;
-    toast.classList.add(`successMsg`);
-  }else{
-    toast.innerHTML = errorMsg;
-    toast.classList.add(`errorMsg`);
-  }
-  
-  toastBox.appendChild(toast);
-}
+function showToast(message, className) {
+    let toast = document.createElement(`div`);
+    toast.classList.add(`toast`);
+    toast.classList.add(className);
+    toast.innerHTML = message;
+
+    toastBox.appendChild(toast);
+
+    const delay = document.querySelector('input[name="delay"]').value;
+    setTimeout(() => {
+        toast.remove();
+    }, delay);
+    
+};
